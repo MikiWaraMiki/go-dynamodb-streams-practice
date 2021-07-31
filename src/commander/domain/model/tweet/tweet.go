865f4f0823e7a247af044c647a3d41a71f900e44
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-07-31 16:55:42 +0900

    add tweet model

--- src/commander/domain/model/tweet/tweet.go
+++ src/commander/domain/model/tweet/tweet.go
@@ -1,5 +1,15 @@
-package domain_model
+package domain_model_tweet
 
 type Tweet struct {
-	Id string
+	id *TweetId
+}
+
+func NewTweet(id *TweetId) *Tweet {
+	return &Tweet{
+		id: id,
+	}
+}
+
+func (t Tweet) Id() string {
+	return t.id.value
 }
