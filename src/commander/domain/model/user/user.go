d856949172d4de57b34e1ee3e90ca14db8c27dee
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-07-31 16:23:37 +0900

    add user model

---
+++ src/commander/domain/model/user/user.go
@@ -0,0 +1,15 @@
+package domain_model_user
+
+type User struct {
+	id *UserID
+}
+
+func NewUser(id *UserID) *User {
+	return &User{
+		id: id,
+	}
+}
+
+func (u User) Id() string {
+	return u.id.value
+}
---
+++ src/commander/domain/model/user/user_id.go
@@ -0,0 +1,16 @@
+package domain_model_user
+
+import "errors"
+
+type UserID struct {
+	value string
+}
+
+func NewUserID(id string) (*UserID, error) {
+	if id == "" {
+		return nil, errors.New("id is required")
+	}
+	return &UserID{
+		value: id,
+	}, nil
+}
