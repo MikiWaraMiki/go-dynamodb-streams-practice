9cc34c15ae7a7ba3eff52c9418cbb05c4ca9ac55
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-08-15 14:45:17 +0900

    fix sql

--- docker/db/mysql/init.d/V1.0.3__create_favorite_tweets_read_model.sql
+++ docker/db/mysql/init.d/V1.0.3__create_favorite_tweets_read_model.sql
@@ -2,15 +2,13 @@ CREATE TABLE
 IF NOT EXISTS `examples`.`faovorite_tweets`
 (
 	id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	user_uuid CHAR
-(26) NOT NULL,
-	tweet_id bigint NOT NULL,
-	content VARCHAR
-(256) NOT NULL,
+	user_uuid VARCHAR (36) NOT NULL,
+	tweet_id VARCHAR (36) NOT NULL,
+	content VARCHAR (256) NOT NULL,
 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT fk_favorite_tweets_required_users_uuid FOREIGN KEY
-(user_uuid) REFERENCES users
-(uuid) ON
-DELETE RESTRICT
+	CONSTRAINT fk_favorite_tweets_required_users_uuid 
+  FOREIGN KEY (user_uuid) 
+  REFERENCES users(uuid) 
+  ON DELETE RESTRICT
 ) ENGINE = InnoDB;
